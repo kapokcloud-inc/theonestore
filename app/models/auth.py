126b71c4594c7b0c44b8aca3072101f937b95a09
老犁 <cjs@kapokcloud.com>
2018-05-21 11:13:43 +0800

    增加后台auth模块相关文件

---
+++ app/models/auth.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    theonestore
+    https://github.com/kapokcloud-inc/theonestore
+    ~~~~~~~~~~~
+    
+    :copyright: © 2018 by the Kapokcloud Inc.
+    :license: BSD, see LICENSE for more details.
+"""
