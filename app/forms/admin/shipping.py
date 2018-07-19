98c819e4208d00d1943f14fcbe923b68ac6615d2
老犁 <cjs@kapokcloud.com>
2018-07-19 09:54:16 +0800

    优化快递配置

--- app/forms/admin/shipping.py
+++ app/forms/admin/shipping.py
@@ -8,38 +8,19 @@
     :license: BSD, see LICENSE for more details.
 """
 from flask_babel import gettext as _
-from flask_wtf import FlaskForm
-from flask_wtf.file import (
-    FileField, 
-    FileAllowed, 
-    FileRequired, 
-    FileStorage
-)
 from flask_uploads import IMAGES
 from wtforms import (
     IntegerField,
     StringField,
     DecimalField,
-    FileField,
-    SelectField,
-    TextAreaField,
-    DateField
+    HiddenField,
+    BooleanField
 )
 from wtforms.validators import (
     Required,
-    InputRequired,
-    Length,
-    EqualTo,
-    NumberRange,
     ValidationError
 )
-
-from app.database import db
-from app.helpers import (
-    render_template, 
-    log_info,
-    toint
-)
+from app.forms import Form
 
 
 class checkFree(object):
@@ -51,27 +32,32 @@ class checkFree(object):
             raise ValidationError(self.message)
 
 
-class ShippingForm(FlaskForm):
+class ShippingForm(Form):
     """快递form"""
-    shipping_id       = IntegerField()
-    shipping_name     = StringField()
-    is_free           = SelectField(
-                            coerce=int,
-                            choices=[(0, _(u'否')), (1, _(u'是'))]
-                        )
-    shipping_amount   = DecimalField(
-                            validators=[checkFree()]
-                        )
-    free_limit_amount = DecimalField(
-                            validators=[checkFree()]
-                        )
-    is_enable         = SelectField(
-                            coerce=int,
-                            choices=[(0, _(u'否')), (1, _(u'是'))]
-                        )
-    is_default        = SelectField(
-                            coerce=int,
-                            choices=[(0, _(u'否')), (1, _(u'是'))]
-                        )
-    sorting           = IntegerField()
+    shipping_id = HiddenField(default=0, validators=[Required(message=_(u'请选择快递id'))])
+
+    shipping_name = StringField(label=_(u'快递名称'), render_kw={'disabled':''})
+
+    is_free = BooleanField(label=_(u'是否免运费'),
+                description=_(u'免运费'),
+                false_values=(0, '0', ''))
+
+    shipping_amount = DecimalField(label=_(u'邮费'),
+                        validators=[Required(message=_(u'必填项')), checkFree()])
+
+    free_limit_amount = DecimalField(label=_(u'满多少包邮'),validators=[checkFree()])
+
+    is_enable = BooleanField(label=_(u'是否启用'),
+                    description=_(u'启用'),
+                    false_values=(0, '0', ''))
+
+    is_default = BooleanField(label=_(u'是否默认快递'),
+                    description=_(u'默认快递'),
+                    false_values=(0, '0', ''))
+
+    sorting = IntegerField(label=_(u'排序'), 
+                default=50,
+                description=_(u'数值越大排序越靠前'),
+                validators=[Required(message=_(u'必填项'))]
+                )
 
