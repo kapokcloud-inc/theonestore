db811f117b60b77cbfccfc0d4d2764b838b63c1a
chenzijie20 <597768562@qq.com>
2018-07-30 16:17:42 +0800

    首页广告、推荐列表、广告列表（pc/index/index.html.j2、views/pc/index.py）

--- app/views/pc/index.py
+++ app/views/pc/index.py
@@ -19,9 +19,10 @@ from flask_babel import gettext as _
 
 from app.database import db
 
-from app.helpers import render_template
+from app.helpers import (log_info,render_template)
 
 from app.services.api.item import ItemStaticMethodsService
+from app.services.api.adv import AdvStaticMethodsService
 
 from app.models.adv import Adv
 
@@ -33,14 +34,12 @@ index = Blueprint('pc.index', __name__)
 def root():
     """pc - 首页"""
 
-    advs = db.session.query(Adv.adv_id, Adv.img, Adv.ttype, Adv.tid, Adv.url).\
-        filter(Adv.ac_id == 1).\
-        filter(Adv.is_show == 1).\
-        order_by(Adv.sorting.desc(), Adv.adv_id.desc()).all()
+    advs = AdvStaticMethodsService.advs({'ac_id':1})
 
     hot_items, pagination       = ItemStaticMethodsService.items({'is_hot':1, 'p':1, 'ps':12})
     recommend_items, pagination = ItemStaticMethodsService.items({'is_recommend':1, 'p':1, 'ps':12})
 
 
     data = {'advs':advs, 'hot_items':hot_items, 'recommend_items':recommend_items}
+    log_info(advs)
     return render_template('pc/index/index.html.j2', **data)
