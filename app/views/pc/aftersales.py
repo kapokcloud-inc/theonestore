936606af1a82500e6a38221190530d366c0d7474
chenzijie20 <597768562@qq.com>
2018-08-07 15:50:05 +0800

    售后详情视图数据调整（views/pc/aftersales.py）

--- app/views/pc/aftersales.py
+++ app/views/pc/aftersales.py
@@ -32,6 +32,10 @@ from app.services.api.aftersales import (
     AfterSalesStaticMethodsService
 )
 
+from app.services.api.order import (
+    OrderStaticMethodsService
+)
+
 from app.forms.api.aftersales import AfterSalesForm
 
 from app.models.aftersales import (
@@ -39,6 +43,9 @@ from app.models.aftersales import (
     AftersalesLogs
 )
 
+from app.models.order import (
+    OrderAddress
+)
 
 aftersales = Blueprint('pc.aftersales', __name__)
 
@@ -79,24 +86,45 @@ def detail(aftersales_id):
     
     log = AftersalesLogs.query.\
             filter(AftersalesLogs.aftersales_id == aftersales.aftersales_id).\
-            order_by(AftersalesLogs.al_id.desc()).first()
+            order_by(AftersalesLogs.al_id.desc()).all()
 
     status_text, action_code = AfterSalesStaticMethodsService.aftersale_status_text_and_action_code(aftersales)
 
-    data = {'aftersales':aftersales, 'log':log, 'status_text':status_text, 'action_code':action_code}
+    order_address = OrderAddress.query.filter(OrderAddress.order_id == aftersales.order_id).first()
+
+    data = {'aftersales':aftersales, 'log':log, 'status_text':status_text, 'action_code':action_code,'order_address':order_address}
     return render_template('pc/aftersales/detail.html.j2', **data)
 
 
-@aftersales.route('/apply/step0')
-def apply_step0():
+@aftersales.route('/apply/step0/<int:order_id>')
+def apply_step0(order_id):
     """pc站 - 申请售后服务-选择产品"""
 
-    return render_template('pc/aftersales/apply_step0.html.j2')
+    if not check_login():
+        session['weixin_login_url'] = request.headers['Referer']
+        return redirect(url_for('api.weixin.login'))
+    uid = get_uid()
+
+    data = OrderStaticMethodsService.detail_page(order_id, uid)
+    #pc端订单详情不支持再次购买，排除掉指令[5]
+    data['code']= list(set(data['code'])-set([5]))
+
+    return render_template('pc/aftersales/apply_step0.html.j2', **data)
 
 
 @aftersales.route('/apply/step1')
 def apply_step1():
     """pc站 - 申请售后服务-第一步"""
+
+    if not check_login():
+        session['weixin_login_url'] = request.headers['Referer']
+        return redirect(url_for('api.weixin.login'))
+    uid = get_uid()
+
+    order_id = toint(request.args.get('order_id', '0'))
+    og_id    = toint(request.args.get('og_id', '0'))
+
+    
 
     return render_template('pc/aftersales/apply_step1.html.j2')
 
