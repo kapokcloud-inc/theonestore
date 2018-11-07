e523eab4d7547b891c831d4112430bee7f503f19
chenzijie20 <597768562@qq.com>
2018-11-07 18:17:48 +0800

    地址列表接口

--- app/views/api/me.py
+++ app/views/api/me.py
@@ -149,11 +149,11 @@ def address_list():
 
     resjson.action_code = 13
 
-    if not check_login():
-        return resjson.print_json(resjson.NOT_LOGIN)
-    uid = get_uid()
+    # if not check_login():
+    #     return resjson.print_json(resjson.NOT_LOGIN)
+    # uid = get_uid()
 
-    address_list = UserAddress.query.filter(UserAddress.uid == uid).order_by(UserAddress.is_default.desc()).all()
+    address_list = UserAddress.query.filter(UserAddress.uid == 1).order_by(UserAddress.is_default.desc()).all()
     
     data = {'address_list': address_list}
     return resjson.print_json(0, u'ok', data)
