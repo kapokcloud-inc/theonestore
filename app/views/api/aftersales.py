cc834d88271c2ac00bb0649b7ddf04c7da44fd58
chenzijie20 <597768562@qq.com>
2018-12-08 09:31:11 +0800

    售后

--- app/views/api/aftersales.py
+++ app/views/api/aftersales.py
@@ -254,14 +254,14 @@ def apply_info():
 
     if order_id <= 0 and og_id <= 0:
         return resjson.print_json(resjson.PARAM_ERROR)
-
+    wtf_form = AfterSalesForm()
     if order_id > 0:
         ascs = AfterSalesCreateService(uid, order_id=order_id, og_id=0, quantity=1, aftersales_type=1, deliver_status=1)
         ret  = ascs._check_order()
         if not ret:
             return resjson.print_json(resjson.SYSTEM_PAGE_NOT_FOUND)
 
-        data = {'order_id':order_id, 'goods_data':ascs.goods_data, 'refunds_amount':ascs.refunds_amount}
+        data = {'wtf_form': wtf_form, 'order_id':order_id, 'goods_data':ascs.goods_data, 'refunds_amount':ascs.refunds_amount}
         return resjson.print_json(0, u'ok', data)
     else:
         aftersales_type = 2
@@ -278,6 +278,6 @@ def apply_info():
             if not ret:
                 return resjson.print_json(resjson.SYSTEM_PAGE_NOT_FOUND)
 
-        data = {'goods_data':ascs.goods_data,'refunds_amount':ascs.refunds_amount, 'order_address':ascs.order_address, 'aftersales_type':aftersales_type}
+        data = {'wtf_form': wtf_form, 'goods_data':ascs.goods_data,'refunds_amount':ascs.refunds_amount, 'order_address':ascs.order_address, 'aftersales_type':aftersales_type}
 
         return resjson.print_json(0, u'ok', data)
