42a1dd524b6d35db06a718f8c2e1b7ac2fc01abc
老犁 <cjs@kapokcloud.com>
2019-03-06 15:44:20 +0800

    增加用户异常、地址异常、订单异常、优惠券异常、商品异常

--- app/exception/__init__.py
+++ app/exception/__init__.py
@@ -3,33 +3,60 @@
     theonestore
     https://github.com/kapokcloud-inc/theonestore
     ~~~~~~~~~~~
-    
     :copyright: © 2018 by the Kapokcloud Inc.
     :license: BSD, see LICENSE for more details.
 """
+from flask_babel import gettext as _
+
+
 class TheonestoreException(Exception):
     """ 一店异常 """
-
     def __init__(self, msg):
         self.msg = msg
-    
+
     def __str__(self):
         return self.msg
 
+
 class ConfigNotFoundException(TheonestoreException):
     """ 配置不存在异常 """
-
-    def __init__(self, msg=u'配置不存在'):
+    def __init__(self, msg=_(u'配置不存在')):
         TheonestoreException.__init__(self, msg)
+
 
 class ParamNotFoundException(TheonestoreException):
     """ 参数不存在异常 """
-
-    def __init__(self, msg=u'参数不存在'):
+    def __init__(self, msg=_(u'参数不存在')):
         TheonestoreException.__init__(self, msg)
+
 
 class NetworkException(TheonestoreException):
     """ 网络异常 """
+    def __init__(self, msg=_(u'网络异常')):
+        TheonestoreException.__init__(self, msg)
 
-    def __init__(self, msg=u'网络异常'):
+
+class UserException(TheonestoreException):
+    """用户异常"""
+    def __init__(self, msg=_(u'用户不存在')):
+        TheonestoreException.__init__(self, msg)
+
+
+class AddressException(TheonestoreException):
+    def __init__(self, msg=_(u'地址不存在')):
+        TheonestoreException.__init__(self, msg)
+
+
+class OrderException(TheonestoreException):
+    def __init__(self, msg=_(u'订单不存在')):
+        TheonestoreException.__init__(self, msg)
+
+
+class CouponException(TheonestoreException):
+    def __init__(self, msg=_(u'优惠券不存在')):
+        TheonestoreException.__init__(self, msg)
+
+
+class GoodsException(TheonestoreException):
+    def __init__(self, msg=_(u'商品不存在')):
         TheonestoreException.__init__(self, msg)
