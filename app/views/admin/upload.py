693df99616149bfe8265da9c2502806569fa70d4
老犁 <cjs@kapokcloud.com>
2019-03-14 09:33:45 +0800

    修正后台ueditor配置问题

--- app/views/admin/upload.py
+++ app/views/admin/upload.py
@@ -28,6 +28,7 @@ from app.services.uploads import FileUploadService
 
 upload = Blueprint('admin.upload', __name__)
 
+
 @upload.route('/ueditor', methods=["GET", "POST"])
 def ueditor():
     """ueditor上传"""
@@ -38,13 +39,14 @@ def ueditor():
     if action == 'config':
         # 解析JSON格式的配置文件
         # 这里使用PHP版本自带的config.json文件
-        config_filename = os.path.join(current_app.static_folder, 'default', 'admin', 'plugins', 'ue', 'php', 'config.json')
+        config_filename = os.path.join(
+            current_app.static_folder, 'default', 'admin', 'plugins', 'ue', 'config.json')
         with open(config_filename) as fp:
             data = fp.read()
             CONFIG = json.loads(re.sub(r'\/\*.*\*\/', '', data))
             # log_info('##########:%s' % CONFIG)
             res.data = json.dumps(CONFIG)
-        
+
         log_info('-----------------')
         return res
 
@@ -52,11 +54,12 @@ def ueditor():
     upfile = request.files['upfile']
 
     try:
-        fus   = FileUploadService()
+        fus = FileUploadService()
         image = fus.save_storage(upfile, 'ueditor')
     except Exception as e:
-        return json.dumps({'state':'FAIL'})
+        return json.dumps({'state': 'FAIL'})
 
-    data = {'state':'SUCCESS', 'source':image, 'url':image, 'title':image, 'original':image}
+    data = {'state': 'SUCCESS', 'source': image,
+            'url': image, 'title': image, 'original': image}
     res.data = json.dumps(data)
     return res
