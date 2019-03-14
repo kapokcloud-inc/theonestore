c1e02ec1233c3e3116cf2582f361548de921957b
老犁 <cjs@kapokcloud.com>
2019-03-14 18:27:21 +0800

    增加“加入购物车”跳转页面

--- app/views/pc/cart.py
+++ app/views/pc/cart.py
@@ -20,7 +20,6 @@ from flask import (
 from flask_babel import gettext as _
 
 from app.database import db
-
 from app.helpers import (
     render_template,
     model_create,
@@ -34,7 +33,6 @@ from app.helpers.user import (
     check_login,
     get_uid
 )
-
 from app.services.api.order import PayService
 from app.services.api.pay_weixin import NativeService
 from app.services.api.cart import (
@@ -42,15 +40,16 @@ from app.services.api.cart import (
     CheckoutService,
     CartStaticMethodsService
 )
-
+from app.models.item import Goods
 
 cart = Blueprint('pc.cart', __name__)
+
 
 @cart.route('/')
 def root():
     """pc - 我的购物车"""
 
-    uid        = get_uid()
+    uid = get_uid()
     session_id = session.sid
 
     msg = request.args.get('msg', '').strip()
@@ -58,9 +57,40 @@ def root():
     cs = CartService(uid, session_id)
     cs.check()
 
-    data = {'msg':msg, 'carts':cs.carts, 'items_amount':cs.items_amount, 'items_quantity':cs.items_quantity,
-            'cart_total':cs.cart_total, 'cart_valid_total':cs.cart_valid_total}
+    data = {
+        'msg': msg,
+        'carts': cs.carts,
+        'items_amount': cs.items_amount,
+        'items_quantity': cs.items_quantity,
+        'cart_total': cs.cart_total,
+        'cart_valid_total': cs.cart_valid_total}
     return render_template('pc/cart/index.html.j2', **data)
+
+
+@cart.route('/add')
+def add():
+    """购物车增加"""
+    args = request.args
+    goods_id = toint(args.get('goods_id', '0'))
+    quantity = toint(args.get('quantity', '1'))
+    goods = Goods.query.get_or_404(goods_id)
+    goods_list = db.session.query(
+        Goods.goods_id, Goods.goods_name, Goods.goods_img,
+        Goods.goods_desc, Goods.goods_price, Goods.sale_count,
+        Goods.fav_count, Goods.comment_count).\
+        filter(Goods.cat_id == goods.cat_id).\
+        filter(Goods.is_delete == 0).\
+        filter(Goods.is_sale == 1).\
+        filter(Goods.stock_quantity > 0).\
+        filter(Goods.goods_id != goods_id).\
+        order_by(Goods.sale_count.desc()).\
+        order_by(Goods.fav_count.desc()).\
+        order_by(Goods.goods_id.desc()).\
+        limit(20).all()
+    return render_template(
+        'pc/cart/add.html.j2',
+        goods=goods,
+        goods_list=goods_list)
 
 
 @cart.route('/checkout')
@@ -89,7 +119,8 @@ def pay(order_id):
         return redirect(url_for('api.weixin.login_qrcode'))
     uid = get_uid()
 
-    ret, msg, data, url = CartStaticMethodsService.pay_page(order_id, uid, 'pc')
+    ret, msg, data, url = CartStaticMethodsService.pay_page(
+        order_id, uid, 'pc')
     if not ret:
         return redirect(url)
 
@@ -101,14 +132,15 @@ def pay(order_id):
     if not ps.tran:
         ps.create_tran()
 
-    tran       = ps.tran
-    tran_id    = tran.tran_id
-    subject    = u'交易号：%d' % tran_id
-    nonce_str  = str(tran_id)
+    tran = ps.tran
+    tran_id = tran.tran_id
+    subject = u'交易号：%d' % tran_id
+    nonce_str = str(tran_id)
     pay_amount = Decimal(tran.pay_amount).quantize(Decimal('0.00'))*100
 
     # 支付二维码
-    ns = NativeService(nonce_str, subject, tran_id, pay_amount, request.remote_addr)
+    ns = NativeService(nonce_str, subject, tran_id,
+                       pay_amount, request.remote_addr)
     if not ns.create_qrcode():
         return redirect(url_for('pc.order.index', msg=ns.msg))
     data['qrcode'] = ns.qrcode
